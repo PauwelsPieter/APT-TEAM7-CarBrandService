71c0d57ad6fa7443b12c5ea808d6308097f8d731
Pieter Pauwels <r0796064@student.thomasmore.be>
2021-11-12 09:58:59 +0100

    Set database

--- src/main/java/fact/it/carbrandservice/controller/BrandController.java
+++ src/main/java/fact/it/carbrandservice/controller/BrandController.java
@@ -3,9 +3,8 @@ package fact.it.carbrandservice.controller;
 import fact.it.carbrandservice.model.Brand;
 import fact.it.carbrandservice.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -31,5 +30,36 @@ public class BrandController {
     @GetMapping("/brands/{country}")
     public List<Brand> getBrandsByCountry(@PathVariable String country) {
         return brandRepository.findBrandsByCountry(country);
+    }
+
+    @PostMapping("/brands")
+    public Brand addBrand(@RequestBody Brand brand) {
+        brandRepository.save(brand);
+        return brand;
+    }
+
+    @PutMapping("/brands")
+    public Brand updateBrand(@RequestBody Brand updatedBrand) {
+        Brand retrievedBrand = brandRepository.findBrandsById(updatedBrand.getId());
+
+        retrievedBrand.setName(updatedBrand.getName());
+        retrievedBrand.setCountry(updatedBrand.getCountry());
+        retrievedBrand.setFoundingYear(updatedBrand.getFoundingYear());
+
+        brandRepository.save(retrievedBrand);
+
+        return retrievedBrand;
+    }
+
+    @DeleteMapping("/brands/{id}")
+    public ResponseEntity deleteBrand(@PathVariable String id) {
+        Brand brand = brandRepository.findBrandsById(id);
+        if (brand != null) {
+            brandRepository.delete(brand);
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/fact/it/carbrandservice/repository/BrandRepository.java
+++ src/main/java/fact/it/carbrandservice/repository/BrandRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface BrandRepository extends MongoRepository<Brand, String> {
     List<Brand> findBrandsByCountry(String country);
+    Brand findBrandsById(String id);
 }
